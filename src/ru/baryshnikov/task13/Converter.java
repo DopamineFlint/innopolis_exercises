a0eeb840d1d6c9282501b07c3d29e506a32ffc35
DopamineFlint <dopamine.flint@gmail.com>
2020-05-17 21:00:30 +0300

    task 13 using Scanner and String.format

---
+++ src/ru/baryshnikov/task13/Converter.java
@@ -0,0 +1,64 @@
+package ru.baryshnikov.task13;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.Scanner;
+
+public class Converter {
+    public static void main(String[] args) throws FileNotFoundException {
+        ArrayList<String> products = new ArrayList<>();
+        ArrayList<Double> price = new ArrayList<>();
+        ArrayList<Double> amount = new ArrayList<>();
+        Formatter form = new Formatter();
+        String s;
+
+        File f = new File("C:\\My Library\\products.txt");
+        Scanner scan = new Scanner(f);
+
+        int a = 0;
+        int i = 0;
+        double totalsum = 0;
+
+        while (scan.hasNextLine()) {
+            System.out.println("==" + i++);
+
+            if (scan.hasNextDouble()) {
+                price.add(scan.nextDouble());
+                amount.add(scan.nextDouble());
+            }
+
+            if (scan.hasNextLine()) {
+                products.add(scan.nextLine());
+            }
+        }
+
+        products.removeAll(Arrays.asList(""));
+        System.out.println(products);
+        System.out.println(price);
+        System.out.println(amount);
+
+        addString(String.format("%-20s%-7s%-10s%-9s\n", "Наименование", "Цена", "Кол-во", "Стоимость"));
+        addString(String.format("================================================\n"));
+        for (a = 0; a < products.size(); a++) {
+            double sum = price.get(a) * amount.get(a);
+            totalsum = totalsum + sum;
+            System.out.printf("%s: %.2f * %.2f = %.2f \n", products.get(a), price.get(a), amount.get(a), sum);
+            addString(String.format("%-18s%6.2f%2s%7.2f%15.2f%n", products.get(a), amount.get(a), "x", price.get(a), sum));
+        }
+        addString("================================================\n");
+        addString(String.format("%-41s%.2f","Итого:" ,totalsum));
+        System.out.printf("Total sum is %.2f", totalsum);
+    }
+
+    public static void addString(String s) {
+        try (FileWriter fw = new FileWriter("C:\\My Library\\receipt.txt",
+                StandardCharsets.UTF_8, true)) {
+            fw.append(s);
+        } catch (IOException e) {
+            e.getStackTrace();
+        }
+    }
+}
